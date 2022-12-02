fcb4ba988ef11277000244db6d9446dbd6735a0c
Andrew <andrewonulak@gmail.com>
2022-12-02 09:20:20 -0700

    commit tree ea746c55808285b2dc4f865cd06ff051c0aef04f

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isListed, setIsListed] = useState(false)
+
+function addToCart(){
+  setIsListed(isListed => !isListed)
+}
+
+const added = isListed ? "in-cart" : ""
+
   return (
-    <li className="">
+    <li className={added}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addToCart}>{added ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
